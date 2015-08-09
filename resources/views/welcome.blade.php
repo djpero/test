edbae19508d7bcb174234e6104bfb89d4ef3884c
Pero Knezevic <djpero.84@gmail.com>
2015-08-09 21:40:41 +0200

    Test git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 53</div>
+                <div class="title">Laravel 599</div>
             </div>
         </div>
     </body>
